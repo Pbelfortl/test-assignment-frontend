72047b9f5f3527af98e8e60f9cff914d9ef0ab47
Pablo Belfort <pablobelfort@gmail.com>
2023-03-25 18:42:27 -0300

    final version

--- src/pages/AddProduct.js
+++ src/pages/AddProduct.js
@@ -10,7 +10,7 @@ export function AddProduct() {
     const [sku, setSku] = useState();
     const [name, setName] = useState();
     const [price, setPrice] = useState();
-    const [type, setType] = useState();
+    const [type, setType] = useState("2");
     const [attValue, setAttValue] = useState();
     const [height, setHeight] = useState()
     const [width, setWidth] = useState();
@@ -63,7 +63,7 @@ export function AddProduct() {
                         <div>
                             <label htmlFor="productType">Type switcher</label>
                             <select id="productType">
-                                <option id="DVD" value="1" label="DVD" onSelect={setType(e.target.value)}>DVD</option>
+                                <option id="DVD" value="1" onSelect={setType(e.target.value)}>DVD</option>
                                 <option id="Book" value="2" onSelect={setType(e.target.value)}>Book</option>
                                 <option id="Furniture" value="3" onSelect={setType(e.target.value)}>Furniture</option>
                             </select>
