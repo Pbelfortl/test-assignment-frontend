07fbcad9a22f2a29e6f64370cd6ca4a86d6f32ad
Pablo Belfort <pablobelfort@gmail.com>
2023-03-26 14:15:39 -0300

    final version

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -76,7 +76,7 @@ export const Header = styled.div`
     justify-content: space-between;
     align-items:center;
     width: 1250px;
-    height: 110px;
+    height: 95x;
     border-bottom: 2px black solid;
     padding-left: 20px;
     button{
